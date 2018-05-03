ba37e6e7e533587321fc5097cd5d6b433f9fe620
singha37 <singha37@g.toiohomai.ac.nz>
2018-05-03 23:53:39 +1200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+
+namespace ques2
+{
+    class Program
+    {
+        public class Person
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public int YearOfBirth {get;set;}
+
+            public Person(string fname, string lname)
+            {
+                FirstName = fname;
+                LastName = lname;
+            }
+
+            public string GetFullName()
+            {
+                string FullName = FirstName + " " + LastName;
+                return FullName;
+            }
+            
+            public int GetAge()
+            {
+                return (2018 - YearOfBirth);
+            }
+        }
+        static void Main(string[] args)
+        {
+            string fname, lname;
+            Console.WriteLine("Enter name of new person below");
+             Console.WriteLine("");
+            Console.Write("First Name: ");
+            fname = (Console.ReadLine());
+            Console.Write("Last Name: ");
+            lname = (Console.ReadLine());
+            Person person = new Person(fname, lname);
+            Console.Write("Enter Year Of Birth: ");
+            person.YearOfBirth = Int32.Parse((Console.ReadLine()));
+            string desc = "Person: " + person.GetFullName() + " is " + person.GetAge() + " years old";
+            Console.WriteLine(desc);
+            Console.ReadKey();
+        }
+    }
+}
